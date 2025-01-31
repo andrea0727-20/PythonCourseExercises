9ba286d8c045af18ddf0aca3c817ceb031a9b18b
JohannaAndrea.MoralesRodriguez <johannaandrea.moralesrodriguez@ticketmaster.com>
2025-01-31 17:29:13 -0500

    adding the exercises 1 and 2 solutions from exercises #2

---
+++ Exercises#2/exercise1.py
@@ -0,0 +1,6 @@
+text = input("Please, enter a text: ")
+
+print("Text with uppercase method: ", text.upper())
+print("Text with lowercase method: ", text.lower())
+print("Text with capitalized method: ", text.capitalize())
+print("Text with capitalized method with the first letters of each word: ", text.title())
---
+++ Exercises#2/exercise2.py
@@ -0,0 +1,24 @@
+def get_boolean_input(prompt):
+    values = {"true": True, "false": False}
+   
+    while (value := input(prompt).strip().lower()):
+        if value in values:
+            return values[value]
+        else:
+            print("Invalid value. Please entter just True or False: ")
+
+value_1= get_boolean_input("Enter the first boolean value True/False: ")
+value_2= get_boolean_input("Enter the second boolean value True/False: ")
+
+# Logic operations assessment 
+print("Results \n")
+print(f"{value_1} AND {value_2} -> {value_1 and value_2}")
+print(f"{value_1} OR {value_2} -> {value_1 or value_2}")
+print(f"NOT {value_1} -> {not value_1}")
+print(f"NOT {value_2} -> {not value_2}")
+      
+
+
+
+
+
